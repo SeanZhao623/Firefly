054a7cc6c18920cf50d875e11608b82f9b2ac4d3
Scciwe <112156887+SeanZhao623@users.noreply.github.com>
2026-08-29 16:58:20 +0800

    Update sidebarConfig.ts

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -19,14 +19,14 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 	tabletSidebar: "left",
 
 	// 文章详情页隐藏侧边栏，设为 true 则只在首页等非文章页显示
-	hideSidebarOnPostPage: false,
+	hideSidebarOnPostPage: true,
 
 	// 文章详情页保持双侧栏
 	// 使用单侧栏(position为left或right)时，是否在文章详情页显示双侧边栏，（hideSidebarOnPostPage需要保持false）
 	// 当position为left时开启此项，文章详情页将额外显示右侧边栏
 	// 当position为right时开启此项，文章详情页将额外显示左侧边栏
 	// 适用在只想用单侧栏，但在文章详情页想用对侧栏的目录等组件的场景
-	showBothSidebarsOnPostPage: false,
+	showBothSidebarsOnPostPage: true,
 
 	// 左侧边栏组件配置列表
 	// 组件的渲染顺序完全取决于它们在配置数组中出现的顺序，但top的组件会优先于sticky位置的组件渲染
